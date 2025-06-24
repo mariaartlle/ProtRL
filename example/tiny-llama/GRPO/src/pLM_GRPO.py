c4ef883664c5bbad08c04b7d22c55ee0034d7f24
Filippo Stocco <148588396+StoccoFilippo@users.noreply.github.com>
2025-06-24 09:18:59 +0200

    Update pLM_GRPO.py

--- example/tiny-llama/GRPO/src/pLM_GRPO.py
+++ example/tiny-llama/GRPO/src/pLM_GRPO.py
@@ -69,6 +69,13 @@ class pLM_GRPOTrainer(GRPOTrainer):
             
         return RandomSampler(self.train_dataset)
 
+    def _get_eval_sampler(self, dataset: Optional[Dataset] = None) -> Sampler:
+
+        if dataset is None:
+            dataset = self.eval_dataset
+            
+        return RandomSampler(self.eval_dataset)
+
     def _generate_and_score_completions(
         self, inputs: list[dict[str, Union[torch.Tensor, Any]]]
         ) -> dict[str, Union[torch.Tensor, Any]]:
