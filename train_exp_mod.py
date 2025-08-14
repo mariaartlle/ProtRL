8069ea5f871b74abfd4e43ed35c4804afb4fd31c
maria.artigues <maria.artigues@upf.edu>
2025-08-14 16:32:07 +0200

    meddling tokenizer

--- train_exp_mod.py
+++ train_exp_mod.py
@@ -99,10 +99,13 @@ eval_dataset   = split['test']
                                         #   add_bos_token=False,
                                         #   use_fast=True)
 
-tokenizer = Tokenizer.from_file('/users/nferruz/martigues/scratch/juan_progen2/FT2_redo/tokenizer_progen2.json')
-fast_tokenizer = PreTrainedTokenizerFast(tokenizer_object=tokenizer)
-fast_tokenizer.eos_token = '<|eos|>'
-fast_tokenizer.pad_token = fast_tokenizer.eos_token
+# tokenizer = Tokenizer.from_file('/users/nferruz/martigues/scratch/juan_progen2/FT2_redo/tokenizer_progen2.json')
+# fast_tokenizer = PreTrainedTokenizerFast(tokenizer_object=tokenizer)
+# fast_tokenizer.eos_token = '<|eos|>'
+# fast_tokenizer.pad_token = fast_tokenizer.eos_token
+
+# try 
+fast_tokenizer = PreTrainedTokenizerFast(tokenizer_file='/users/nferruz/martigues/scratch/juan_progen2/FT2_redo/tokenizer_progen2.json')
 
 device = 'cuda:0'
 
